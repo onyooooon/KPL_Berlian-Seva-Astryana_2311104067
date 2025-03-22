66838d034e659264cae2924a58cb4a282ad13b24
onyooooon <berlianseva12@gmail.com>
2025-03-23 00:59:57 +0700

    implementasi-generic-method

---
+++ 05_Design_by_Contract_Assertions_Defensive_Programming/JURNAL5/modul5_2311104067/Penjumlahan.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace modul5_2311104067
+{
+    class Penjumlahan
+    {
+        static void Main()
+        {
+            Penjualan<int> penjumlahan = new Penjumlahan<int>();
+            int hasil = penjumlahan.JumlahTigaAngka(12, 34, 56);
+            Console.Writeline($"Hasil Penjumlahan: {hasil}");
+
+            SimpleDataBase<int> database = new SimpleDataBase<int>();
+            database.AddNewData(12);
+            database.AddNewData(34);
+            database.AddNewData(56);
+            database.PrintAllData();
+        }
+    }
+}
