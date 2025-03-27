bdf8eb5b9b868c261d53459e5d4aaad190c9b71b
onyooooon <berlianseva12@gmail.com>
2025-03-27 20:48:47 +0700

    menambah folder

---
+++ 05_Design_by_Contract_Assertions_Defensive_Programming/JURNAL5/modul5_2311104067/Penjumlahan.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+
+class Penjumlahan<T>
+{
+    public T JumlahTigaAngka(T a, T b, T c)
+    {
+        dynamic x = a;
+        dynamic y = b;
+        dynamic z = c;
+        return x + y + z;
+    }
+}
