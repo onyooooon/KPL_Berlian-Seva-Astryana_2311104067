5a2e7630121d1dd2fd04c2d4231706957614e720
onyooooon <berlianseva12@gmail.com>
2025-03-28 02:28:38 +0700

    update

---
+++ 05_Design_by_Contract_Assertions_Defensive_Programming/TP/tpmodul6_2311104067/tpmodul6_2311104067/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        SayaTubeVideo video = new SayaTubeVideo("Tutorial Design By Contract – Berlian Seva Astryana");
+        video.PrintVideoDetails();
+
+        Console.WriteLine("\nMenambahkan play count...");
+        video.IncreasePlayCount(10000000); // Tambah 10 juta
+        video.PrintVideoDetails();
+
+        Console.WriteLine("\nMenguji Overflow...");
+        for (int i = 0; i < 25; i++) // Loop untuk menambah play count sampai overflow
+        {
+            video.IncreasePlayCount(10000000);
+        }
+
+        video.PrintVideoDetails();
+    }
+}
