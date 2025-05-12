a31845d41fa7fc34746983c8e48c76f4cb5e66f5
onyooooon <berlianseva12@gmail.com>
2025-05-12 19:39:09 +0700

    tp modul 12

---
+++ 11_Performance_Analysis_Unit_Testing_Debugging/tpmodul12_2311104067/tpmodul12_2311104067/Form1.Designer.cs
@@ -0,0 +1,84 @@
+﻿namespace tpmodul12_2311104067
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBoxInput = new System.Windows.Forms.TextBox();
+            this.buttonCek = new System.Windows.Forms.Button();
+            this.labelOutput = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // textBoxInput
+            // 
+            this.textBoxInput.Location = new System.Drawing.Point(179, 32);
+            this.textBoxInput.Name = "textBoxInput";
+            this.textBoxInput.Size = new System.Drawing.Size(100, 20);
+            this.textBoxInput.TabIndex = 0;
+            // 
+            // buttonCek
+            // 
+            this.buttonCek.Location = new System.Drawing.Point(192, 58);
+            this.buttonCek.Name = "buttonCek";
+            this.buttonCek.Size = new System.Drawing.Size(75, 23);
+            this.buttonCek.TabIndex = 1;
+            this.buttonCek.Text = "Cek";
+            this.buttonCek.UseVisualStyleBackColor = true;
+            this.buttonCek.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // labelOutput
+            // 
+            this.labelOutput.AutoSize = true;
+            this.labelOutput.Location = new System.Drawing.Point(226, 106);
+            this.labelOutput.Name = "labelOutput";
+            this.labelOutput.Size = new System.Drawing.Size(0, 13);
+            this.labelOutput.TabIndex = 2;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.labelOutput);
+            this.Controls.Add(this.buttonCek);
+            this.Controls.Add(this.textBoxInput);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBoxInput;
+        private System.Windows.Forms.Button buttonCek;
+        private System.Windows.Forms.Label labelOutput;
+    }
+}
+
---
+++ 11_Performance_Analysis_Unit_Testing_Debugging/tpmodul12_2311104067/tpmodul12_2311104067/Form1.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace tpmodul12_2311104067
+{
+    public partial class Form1: Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+        private string CariTandaBilangan(int a)
+        {
+            if (a < 0)
+                return "Negatif";
+            else if (a > 0)
+                return "Positif";
+            else
+                return "Nol";
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (int.TryParse(textBoxInput.Text, out int input))
+            {
+                string hasil = CariTandaBilangan(input);
+                labelOutput.Text = hasil;
+            }
+            else
+            {
+                labelOutput.Text = "Input tidak valid!";
+            }
+        }
+    }
+}
