ccd28ad3d712e60ff0cac9dc6f7818285c492098
onyooooon <berlianseva12@gmail.com>
2025-05-15 14:43:42 +0700

    tp modul 13

---
+++ 11_Performance_Analysis_Unit_Testing_Debugging/TestProject1/HelperTest.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using tpmodul12_2311104067;
+
+namespace TestProject1
+{
+    [TestClass]
+    public class HelperTest
+    {
+        [TestMethod]
+        public void Test_Positive()
+        {
+            string result = Helper.CariTandaBilangan(5);
+            Assert.AreEqual("Positif", result);
+        }
+
+        [TestMethod]
+        public void Test_Negative()
+        {
+            string result = Helper.CariTandaBilangan(-3);
+            Assert.AreEqual("Negatif", result);
+        }
+
+        [TestMethod]
+        public void Test_Zero()
+        {
+            string result = Helper.CariTandaBilangan(0);
+            Assert.AreEqual("Nol", result);
+        }
+    }
+}
---
+++ 11_Performance_Analysis_Unit_Testing_Debugging/tpmodul12_2311104067/tpmodul12_2311104067/Helper.cs
@@ -0,0 +1,16 @@
+﻿// Helper.cs
+namespace tpmodul12_2311104067
+{
+    public static class Helper
+    {
+        public static string CariTandaBilangan(int a)
+        {
+            if (a < 0)
+                return "Negatif";
+            else if (a > 0)
+                return "Positif";
+            else
+                return "Nol";
+        }
+    }
+}
