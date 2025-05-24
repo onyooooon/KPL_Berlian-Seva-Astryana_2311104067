7475475a3b7e42f1aa7bb6bc2ca12ae93644da9b
onyooooon <berlianseva12@gmail.com>
2025-05-25 00:57:34 +0700

    menambahkan API mahasiswa

---
+++ 08_API_Design_and_Usage/tpmodul9_2311104067/tpmodul9_2311104067/Controllers/MahasiswaController.cs
@@ -0,0 +1,51 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+
+namespace tpmodul9_2311104067.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class MahasiswaController : ControllerBase
+    {
+        private static List<Mahasiswa> daftarMahasiswa = new List<Mahasiswa>
+        {
+            new Mahasiswa { Nama = "Berlian Seva Astryana", Nim = "2311104067" },
+            new Mahasiswa { Nama = "Dimastian Aji Wibowo", Nim = "2311104058" },
+            new Mahasiswa { Nama = "Fahmi Hasan Asagaf", Nim = "2311104074" }
+        };
+
+        // GET /api/mahasiswa
+        [HttpGet]
+        public ActionResult<List<Mahasiswa>> GetAll()
+        {
+            return daftarMahasiswa;
+        }
+
+        // GET /api/mahasiswa/{index}
+        [HttpGet("{index}")]
+        public ActionResult<Mahasiswa> GetByIndex(int index)
+        {
+            if (index < 0 || index >= daftarMahasiswa.Count)
+                return NotFound();
+
+            return daftarMahasiswa[index];
+        }
+
+        [HttpPost]
+        public ActionResult Add(Mahasiswa mhs)
+        {
+            daftarMahasiswa.Add(mhs);
+            return Ok();
+        }
+
+        [HttpDelete("{index}")]
+        public ActionResult Delete(int index)
+        {
+            if (index < 0 || index >= daftarMahasiswa.Count)
+                return NotFound();
+
+            daftarMahasiswa.RemoveAt(index);
+            return Ok();
+        }
+    }
+}
---
+++ 08_API_Design_and_Usage/tpmodul9_2311104067/tpmodul9_2311104067/Mahasiswa.cs
@@ -0,0 +1,8 @@
+﻿namespace tpmodul9_2311104067
+{
+    public class Mahasiswa
+    {
+        public string Nama { get; set; }
+        public string Nim { get; set; }
+    }
+}
