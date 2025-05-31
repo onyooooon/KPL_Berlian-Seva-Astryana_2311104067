3afe497d3471a071e908104d819350ebdb194a1e
onyooooon <berlianseva12@gmail.com>
2025-06-01 03:16:42 +0700

    menambahkan tugas pendahuluan

---
+++ 09_Code_Reuse_and_Libraries/tpmodul10_2311104067/AljabarConsoleApp1/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using AljabarLibraries;
+
+namespace AljabarConsoleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Aljabar aljabar = new Aljabar();
+
+            // Contoh Akar Persamaan Kuadrat
+            double[] persamaanKuadrat = { 1, -3, -10 };
+            double[] hasilAkar = aljabar.AkarPersamaanKuadrat(persamaanKuadrat);
+            Console.WriteLine("Akar-akar dari persamaan kuadrat:");
+            foreach (var akar in hasilAkar)
+            {
+                Console.WriteLine(akar);
+            }
+
+            // Contoh Hasil Kuadrat
+            double[] persamaanLinear = { 2, -3 };
+            double[] hasilKuadrat = aljabar.HasilKuadrat(persamaanLinear);
+            Console.WriteLine("\nHasil kuadrat dari persamaan linear:");
+            foreach (var nilai in hasilKuadrat)
+            {
+                Console.WriteLine(nilai);
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ 09_Code_Reuse_and_Libraries/tpmodul10_2311104067/tpmodul10_2311104067/Aljabar.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace AljabarLibraries
+{
+    public class Aljabar
+    {
+        public double[] AkarPersamaanKuadrat(double[] persamaan)
+        {
+            double a = persamaan[0];
+            double b = persamaan[1];
+            double c = persamaan[2];
+            double diskriminan = b * b - 4 * a * c;
+
+            if (diskriminan < 0)
+            {
+                throw new ArgumentException("Tidak memiliki akar real.");
+            }
+
+            double akar1 = (-b + Math.Sqrt(diskriminan)) / (2 * a);
+            double akar2 = (-b - Math.Sqrt(diskriminan)) / (2 * a);
+
+            return new double[] { akar1, akar2 };
+        }
+
+        public double[] HasilKuadrat(double[] persamaan)
+        {
+            double a = persamaan[0];
+            double b = persamaan[1];
+
+            double hasilA = a * a;
+            double hasilB = 2 * a * b;
+            double hasilC = b * b;
+
+            return new double[] { hasilA, hasilB, hasilC };
+        }
+    }
+}
