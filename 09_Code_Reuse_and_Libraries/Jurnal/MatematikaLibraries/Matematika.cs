f7a7ac21477438b386c9f7de41cc892a269fce0b
onyooooon <berlianseva12@gmail.com>
2025-06-01 16:12:35 +0700

    menambahkan jurnal

---
+++ 09_Code_Reuse_and_Libraries/Jurnal/MatematikaLibraries/Matematika.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Text;
+
+namespace MatematikaLibraries
+{
+    public class Matematika
+    {
+        public int FPB(int a, int b)
+        {
+            while (b != 0)
+            {
+                int temp = b;
+                b = a % b;
+                a = temp;
+            }
+            return a;
+        }
+
+        public int KPK(int a, int b)
+        {
+            return (a * b) / FPB(a, b);
+        }
+
+        public string Turunan(int[] koefisien)
+        {
+            StringBuilder hasil = new StringBuilder();
+            int pangkat = koefisien.Length - 1;
+
+            for (int i = 0; i < koefisien.Length - 1; i++)
+            {
+                int coef = koefisien[i];
+                int pangkatBaru = pangkat - 1;
+                int hasilCoef = coef * pangkat;
+
+                if (hasilCoef == 0) { pangkat--; continue; }
+
+                if (hasil.Length > 0 && hasilCoef > 0)
+                    hasil.Append(" + ");
+                else if (hasilCoef < 0)
+                    hasil.Append(" - ");
+
+                hasil.Append(Math.Abs(hasilCoef));
+                if (pangkatBaru > 0)
+                    hasil.Append("x" + (pangkatBaru == 1 ? "" : pangkatBaru));
+                pangkat--;
+            }
+
+            return hasil.ToString();
+        }
+
+        public string Integral(int[] koefisien)
+        {
+            StringBuilder hasil = new StringBuilder();
+            int pangkat = koefisien.Length;
+
+            for (int i = 0; i < koefisien.Length; i++)
+            {
+                double coef = (double)koefisien[i] / (pangkat - i);
+                if (hasil.Length > 0 && coef > 0)
+                    hasil.Append(" + ");
+                else if (coef < 0)
+                    hasil.Append(" - ");
+
+                hasil.Append(Math.Abs(coef));
+                hasil.Append("x" + (pangkat - i));
+            }
+
+            hasil.Append(" + C");
+            return hasil.ToString();
+        }
+    }
+}
---
+++ 09_Code_Reuse_and_Libraries/Jurnal/Modul10App/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using MatematikaLibraries;
+
+class Program
+{
+    static void Main()
+    {
+        Matematika mtk = new Matematika();
+
+        Console.WriteLine("FPB dari 60 dan 45: " + mtk.FPB(60, 45));
+        Console.WriteLine("KPK dari 12 dan 8: " + mtk.KPK(12, 8));
+        Console.WriteLine("Turunan dari x^3 + 4x^2 -12x + 9: " + mtk.Turunan(new int[] { 1, 4, -12, 9 }));
+        Console.WriteLine("Integral dari 4x^3 + 6x^2 – 12x + 9: " + mtk.Integral(new int[] { 4, 6, -12, 9 }));
+    }
+}
